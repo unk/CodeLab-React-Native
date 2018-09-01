e634dcbd87f7e6c75ffd52efd02f1ed6a656e82b
Juan el Pionero <juanlee0@naver.com>
2018-09-01 12:43:06 +0900

    Update App.js to add counter

--- MyBootstrap/App.js
+++ MyBootstrap/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import MyButton from "./MyButton";
+import MyCounter from './MyCounter';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
+				<MyCounter />
 				<MyButton
 					label={'Label Text'}
 					onPress={() => Alert.alert( 'Title', 'message' )}
